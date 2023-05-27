61f8b2572b8ab1679d9abe5b6ab5308ca4ef8cc2
Dharini Dutia <dharini@openrobotics.org>
2023-05-27 01:26:27 +0000

    run any command in dirs
    Signed-off-by: Dharini Dutia <dharini@openrobotics.org>

--- run_command.sh
+++ run_command.sh
@@ -3,12 +3,25 @@
 echo "enter command to run "
 read command
 
-for directory in */; do
+echo "enter all directories to run $command in"
+echo "relative paths separated by [space] or * for all "
+read line
+if [ "$line" != "*" ] 
+then
+  list=(${line})
+  for directory in ${list[@]}; do
     echo "$directory"
     cd $directory
     $command
     echo " "
     cd ../
-done
-
-
+  done
+else
+  for directory in */; do
+    echo "$directory"
+    cd $directory
+    $command
+    echo " "
+    cd ../
+  done
+fi
